9989119724faf0a74945ada6c0c1c7183bdf3ada
Garrett Hudson <garretthudson87@yahoo.com>
2020-11-06 17:45:50 -0500

    created boiler plate pages and set up the react router to work within the navbar

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,13 +1,29 @@
 import React from 'react'
-
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 // Components
 import Header from './components/Header'
 import Footer from './components/Footer'
 
+// Pages
+import Home from './pages/Home'
+import About from './pages/About'
+import Resume from './pages/Resume'
+import Portfolio from './pages/Portfolio'
+import Contact from './pages/Contact'
+
 function App() {
   return (
     <div className="App">
-      <Header />
+      <Router>
+        <div>
+          <Header />
+          <Route exact path="/" component={Home} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/resume" component={Resume} />
+          <Route exact path="/portfolio" component={Portfolio} />
+          <Route exact path="/contact" component={Contact} />
+        </div>
+      </Router>
       <Footer />
     </div>
   );
--- react-portfolio/src/components/Header.js
+++ react-portfolio/src/components/Header.js
@@ -1,29 +1,37 @@
-import React from 'react';
+import React from 'react'
+import { Link, useLocation } from "react-router-dom";
 
 // Components
 
 
 function Header() {
+    const location = useLocation();
+
+    const font = {
+        fontFamily: 'Arial, Helvetica, sans-serif',
+        fontWeight: '600',
+        fontSize: '1.2rem',
+        textDecoration: 'none'
+    }
  
     return (
-       <>
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-        <a className="navbar-brand" href="/">Garrett Hudson</a>
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark" style={font}>
+        <Link to="/" className={location.pathname === "/" ? "nav-link active navbar-brand" : "nav-link navbar-brand"} style={{fontSize: '2rem'}}>
+            Garrett Hudson
+        </Link>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
             <div className="navbar-nav">
-            <a className="nav-item nav-link" href="/About">About</a>
-            <a className="nav-item nav-link" href="/resume">Resue</a>
-            <a className="nav-item nav-link" href="/portfolio">portfolio</a>
-            <a className="nav-item nav-link" href="/contact">Contact</a>
+            <Link to="/about" className={location.pathname === "/about" ? "nav-link active" : "nav-link"}>About</Link>
+            <Link to="/resume" className={location.pathname === "/resume" ? "nav-link active" : "nav-link"}>Resume</Link>
+            <Link to="/portfolio" className={location.pathname === "/portfolio" ? "nav-link active" : "nav-link"}>Portfolio</Link>
+            <Link to="/contact" className={location.pathname === "/contact" ? "nav-link active" : "nav-link"}>Contact</Link>
             </div>
         </div>
-        </nav>
-       </>
+    </nav>
     )
 }
 
-export default Header;
-
+export default Header
